e0e708e61f278b576e4ff6f56a7bdfbd93001919
Артур <Suppermen1012@gmail.com>
2022-02-24 20:40:46 +0300

    holodos

---
+++ Fridge.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace Holodos
+{
+    class Fridge
+    {
+        private string food;
+        public string Food
+        {
+            get
+            {
+                return food + " Заморожен!";  //I'm freeze your soul!
+            }
+            set
+            {
+                food = value;
+            }
+        }
+        private int time = 0;
+        public int Time
+        {
+            get
+            {
+                return time;
+            }
+            set
+            {
+                if (value <= 60 && value >= 0)
+                {
+                    time = value * 1000;
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+        public void Freezing()
+        {
+            Console.WriteLine($"Вы положили {food} ");
+            Thread.Sleep(time);
+            Console.WriteLine(Food);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Holodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fridge fridge = new Fridge();
+            Console.WriteLine("Какую еду вы хотите положить в холодильник?");
+            string f = Console.ReadLine();
+            fridge.Food = f;
+            fridge.Time = 1;
+            fridge.Freezing();
+            Console.ReadKey();
+        }
+    }
+}
